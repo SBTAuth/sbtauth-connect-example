f2ee315312fa955eaabe64f80e877ea4248bd0eb
xijieyin <xijieyin@gmail.com>
2022-10-19 15:11:29 +0800

    chore: initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,65 @@
+# vanilla-example
+
+## 0.0.10
+
+### Patch Changes
+
+- @safematrix-auth/sbt-auth@0.1.14
+
+## 0.0.9
+
+### Patch Changes
+
+- @safematrix-auth/sbt-auth@0.1.13
+
+## 0.0.8
+
+### Patch Changes
+
+- Updated dependencies [1edf9e1]
+  - @safematrix-auth/sbt-auth@0.1.12
+
+## 0.0.7
+
+### Patch Changes
+
+- @safematrix-auth/sbt-auth@0.1.11
+
+## 0.0.6
+
+### Patch Changes
+
+- @safematrix-auth/sbt-auth@0.1.10
+
+## 0.0.5
+
+### Patch Changes
+
+- @safematrix-auth/sbt-auth@0.1.9
+
+## 0.0.4
+
+### Patch Changes
+
+- @safematrix-auth/sbt-auth@0.1.8
+
+## 0.0.3
+
+### Patch Changes
+
+- Updated dependencies
+  - @safematrix-auth/sbt-auth@0.1.7
+
+## 0.0.2
+
+### Patch Changes
+
+- emit disconnect event
+  - @safematrix-auth/sbt-auth@0.1.6
+
+## 0.0.1
+
+### Patch Changes
+
+- emit accountsChanged event when disconnect
+  - @safematrix-auth/sbt-auth@0.1.5
---
+++ src/main.ts
@@ -0,0 +1,67 @@
+import { SbtAuthWallet } from '@sbtauth/sbtauth-wallet'
+import { ethers } from 'ethers'
+import './style.css'
+
+// Initialize sbtauth
+const sbtauth = new SbtAuthWallet({
+	developMode: true,
+	defaultChainId: '0x4',
+	targetUrl: 'https://localhost:4000'
+})
+
+const connectButton = document.querySelector('#connect')
+const connectLoading = document.querySelector('#connect-loading') as HTMLElement
+connectButton?.addEventListener('click', async () => {
+	connectLoading.hidden = false
+	await sbtauth.login()
+	connectLoading.hidden = true
+	window.alert('Login successfully')
+})
+
+const logoutButton = document.querySelector('#logout')
+const logoutLoading = document.querySelector('#logout-loading') as HTMLElement
+logoutButton?.addEventListener('click', async () => {
+	logoutLoading.hidden = false
+	await sbtauth.logout()
+	logoutLoading.hidden = true
+})
+
+const disconnectButton = document.querySelector('#disconnect')
+disconnectButton?.addEventListener('click', () => {
+	sbtauth.disconnect()
+})
+
+const getUserInfoButton = document.querySelector('#getUserInfo')
+getUserInfoButton?.addEventListener('click', () => {
+	const user = sbtauth.getUserInfo()
+	window.alert(JSON.stringify(user))
+})
+
+const getAccountButton = document.querySelector('#getAccount')
+getAccountButton?.addEventListener('click', () => {
+	if (!sbtauth) return
+	const address = sbtauth.provider?.accounts[0]
+	window.alert(address)
+})
+
+const getBalanceButton = document.querySelector('#getBalance')
+getBalanceButton?.addEventListener('click', async () => {
+	console.log(sbtauth.provider)
+	if (!sbtauth.provider) return
+	const provider = new ethers.providers.Web3Provider(sbtauth.provider)
+	const address = sbtauth.provider.accounts[0]
+	const balance = await provider.getBalance(address)
+	window.alert(balance)
+})
+
+const signMessageButton = document.querySelector('#signMessage')
+signMessageButton?.addEventListener('click', async () => {
+	console.log(sbtauth)
+
+	if (!sbtauth.provider) return
+	const provider = new ethers.providers.Web3Provider(sbtauth.provider)
+	const signer = provider.getSigner()
+	console.log(signer.getAddress())
+	const signature = await signer.signMessage('Test messsage')
+	window.alert(signature)
+})
